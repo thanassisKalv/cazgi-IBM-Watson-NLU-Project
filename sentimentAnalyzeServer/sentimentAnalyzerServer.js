3205be91ed072da2b888c0602a59c25e4644ccc8
thanassisKalv <thanassiskalv@gmail.com>
2021-03-20 21:56:19 +0000

    lab updates

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,6 +6,29 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+
+    return naturalLanguageUnderstanding;
+}
+
+
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
@@ -27,7 +50,7 @@ app.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
 
-let server = app.listen(8080, () => {
+let server = app.listen(8800, () => {
     console.log('Listening', server.address().port)
 })
 
